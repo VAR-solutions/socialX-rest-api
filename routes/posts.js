139162f9d7d6f513041232fd20512f22bb2b34a7
Ravi Varshney <ravivarshney49@gmail.com>
2019-10-28 18:02:31 +0530

    added delete and update post

--- routes/posts.js
+++ routes/posts.js
@@ -7,4 +7,6 @@ router.get('/:post_id', postController.getById);
 router.post('/:post_id/comment', postController.addNewComment);
 router.post('/:post_id/like', postController.like);
 router.post('/:post_id/unlike', postController.unlike);
+router.delete('/:post_id/delete', postController.deletePost);
+router.put('/:post_id/update', postController.updatePost);
 module.exports = router;
